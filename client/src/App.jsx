7c17cfeffcf721a356ddaefa458f633f55d5df40
Azadeh Galidari <agalidari@gmail.com>
2023-06-07 14:58:02 +0200

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { logo } from './assets';
+
+// import { Home, Login, register } from './pages'
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  )
+}
+
+export default App

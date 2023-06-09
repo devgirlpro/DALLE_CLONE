8c395ff309070c069b88d66f9a94fdb511648f13
Azadeh Galidari <agalidari@gmail.com>
2023-06-09 18:52:15 +0200

    Home page layout is done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,7 +24,13 @@ function App() {
                     Create
                 </Link>
             </header>
-            <Routes>{/* <Route link='' element={< />}/> */}</Routes>
+          <main className="w-full sm:p-8 px-4 py-8 bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/create-post' element={<CreatePost />} />
+            </Routes>
+
+          </main>
         </BrowserRouter>
     );
 }

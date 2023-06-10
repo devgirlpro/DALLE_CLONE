d0abe5ee4d6e62576b83076723926c2a5f94ae81
Azadeh Galidari <agalidari@gmail.com>
2023-06-10 16:02:28 +0200

    added CreatePost component and utils

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+import { FormField, Loader } from "../components";
 
 function CreatePost() {
-  return (
-    <div>CreatePost</div>
-  )
+    const navigate = useNavigate();
+    const [form, setForm] = useState({
+        name: "",
+        propmt: "",
+        photo: "",
+    });
+    const [generatingImg, setGeneratingImg] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    return (
+        <section className="max-w-7x1 mx-auto">
+            <div>
+                <div>
+                    <h1 className="font-extrabold text-[#222328] text-[32px]">
+                        Create
+                    </h1>
+                    <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">
+                        Generate an imaginative image through DALL-E AI and
+                        share it with the community
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
 }
 
-export default CreatePost
+export default CreatePost;

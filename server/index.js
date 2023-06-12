f7ede6b78f088e455451fa392f8d720911afd71d
Azadeh Galidari <agalidari@gmail.com>
2023-06-12 17:25:20 +0200

    conected to mongodb

--- server/index.js
+++ server/index.js
@@ -1,21 +1,28 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+import connectDB from "./mongodb/connect.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
 
-app.get('/', async (req, res) => {
-    res.send('Hello fro DALL-E!')
+app.get("/", async (req, res) => {
+    res.send("Hello fro DALL-E!");
 });
 
-const startServer = async() => {
-    app.listen(8080, () => {
-        console.log('server has start on port http://localhost:8080')
-    })
+const startServer = async () => {
+    try {
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080, () =>
+            console.log("server has start on port http://localhost:8080")
+        );
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 startServer();
